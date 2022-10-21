161b78cdad9842dea6bd31065c11dc76ec93f8d7
bethdevopsbunny <bethdevopsbunny@protonmail.com>
2022-10-21 07:37:24 +0100

    !feat: refactor funcation calls

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ but that doesnt mean i cant have fun with it!
 for all data types it can return you the legnth of the encrypted value.
 
 for boolean however, as it can only be true or false having the length of the value allows you to assume
-its encrypted value as true has 4 characters and false has 5
+its encrypted value as true has 4 characters and false has 5.
+
+after re-reading the bug linked above, it shows that  
+
+
 
 here is the age encrypted file in sopsdata/secrets.enc.yml being predicted. 
-![image](https://user-images.githubusercontent.com/80027170/196891904-e1c592e9-88b6-464e-b7ee-2e835dd91cc1.png)
+![image](https://user-images.githubusercontent.com/80027170/196878553-e9782c52-c507-4b59-90f8-277ff002b661.png)
+
--- cmd/run.go
+++ cmd/run.go
@@ -19,23 +19,31 @@ var runCmd = &cobra.Command{
 	Long:  `Print the version number of sops-predictor`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		out := ReadEachLine(args[0])
+		lines := ReadEachLine(args[0])
 
-		for _, i := range out {
-			if i == "sops:" {
+		for _, line := range lines {
+			// sops places a management object at the bottom of its yaml
+			// none of this is important so I just exit the application here.
+			if line == "sops:" {
 				os.Exit(1)
 			}
-			sizeofdata := dataCount(i)
-			if itemType(i) == "boolean" {
-				switch sizeofdata {
-				case 4:
-					fmt.Printf("%s : True\n", getKey(i))
-				case 5:
-					fmt.Printf("%s : False\n", getKey(i))
-				}
 
-			} else {
-				fmt.Printf("%s : %d \n", getKey(i), sizeofdata)
+			values := parseLine(line)
+
+			values.unencryptedLength = dataCount(values.data)
+
+			switch values._type {
+			case "bool":
+				switch values.unencryptedLength {
+				case 4:
+					fmt.Printf("%s:true:bool \n", values.name)
+				case 5:
+					fmt.Printf("%s:false:bool \n", values.name)
+				}
+			case "str":
+				fmt.Printf("%s:%d:string \n", values.name, values.unencryptedLength)
+			case "int":
+				fmt.Printf("%s:%d:int \n", values.name, values.unencryptedLength)
 			}
 
 		}
@@ -43,29 +51,12 @@ var runCmd = &cobra.Command{
 	},
 }
 
-func itemType(field string) string {
-	split := strings.Split(field, ",")
+//
+//		import
+//
+//
 
-	for _, i := range split {
-
-		if strings.HasPrefix(i, "type:") {
-
-			secondSplit := strings.Split(i, ":")
-
-			switch secondSplit[1] {
-			case "bool]":
-				return "boolean"
-			case "str]":
-				return "string"
-			case "int]":
-				return "int"
-			}
-
-		}
-	}
-	return ""
-}
-
+//ReadEachLine standard read text file funcation
 func ReadEachLine(filepath string) (fileLines []string) {
 
 	readFile, err := os.Open(filepath)
@@ -85,39 +76,24 @@ func ReadEachLine(filepath string) (fileLines []string) {
 	return fileLines
 }
 
-func dataCount(field string) int {
-	split := strings.Split(field, ",")
+//
+//		Math
+//
+//
 
-	for _, i := range split {
+//dataCount simple maths function that applies the pattern observed in the unenrypted string.
+func dataCount(data string) int {
 
-		if strings.HasPrefix(i, "data:") {
-
-			encryptedString := strings.Trim(i, "data:")
-
-			item := item{
-				fullLength:  len(encryptedString),
-				paddingSize: getPaddingSize(encryptedString),
-				grouping:    getGrouping(len(encryptedString)),
-			}
-
-			return item.fullLength - (item.grouping + item.paddingSize)
-
-		}
-	}
-	return 0
+	return len(data) - (getGrouping(len(data)) + getPaddingSize(data))
 }
 
+//getGrouping applies the pattern applied by sops that increased the difference between the unencrypted string
+//and the encrypted one. explained in the readme.
 func getGrouping(length int) int {
 	return length / 4
 }
 
-type item struct {
-	fullLength      int
-	paddingSize     int
-	paddinglessSize int
-	grouping        int
-}
-
+//getPaddingSize the full encrypted string contains padding that helps identify where the encrypted string sits in the 3 set
 func getPaddingSize(encryptedString string) int {
 
 	if strings.HasSuffix(encryptedString, "==") {
@@ -131,7 +107,63 @@ func getPaddingSize(encryptedString string) int {
 	return 0
 }
 
+//
+//		Data
+//
+//
+
+type EncryptedString struct {
+	name              string
+	encryption        string
+	data              string
+	iv                string
+	tag               string
+	_type             string
+	unencryptedLength int
+}
+
+//parseLine manages raw text line to object conversion.
+//with below helper functions
+func parseLine(encryptedString string) EncryptedString {
+
+	encryptedStringObject := EncryptedString{
+		name:       getKey(encryptedString),
+		encryption: getEncryption(encryptedString),
+		data:       getData(encryptedString, "data:"),
+		iv:         getData(encryptedString, "iv:"),
+		tag:        getData(encryptedString, "tag:"),
+		_type:      getData(encryptedString, "type:"),
+	}
+
+	return encryptedStringObject
+
+}
+
 func getKey(encryptedString string) string {
 	split := strings.Split(encryptedString, ":")
 	return split[0]
+}
+
+func getEncryption(encryptedString string) string {
+	split := strings.Split(encryptedString, ",")
+	split = strings.Split(split[0], "[")
+	return split[0]
+}
+
+func getData(encryptedString string, dataToCollect string) string {
+	split := strings.Split(encryptedString, ",")
+
+	for _, i := range split {
+
+		if strings.HasPrefix(i, dataToCollect) {
+
+			//clean the end of the type value
+			if dataToCollect == "type:" {
+				i = strings.TrimSuffix(i, "]")
+			}
+			return strings.Trim(i, dataToCollect)
+		}
+
+	}
+	return ""
 }
